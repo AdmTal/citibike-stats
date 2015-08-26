89432485f0ef6adff8248d5f5511224d6cc088af
Adam Tal <admtal@gmail.com>
2015-08-25 20:30:44 -0400

    Adding endpoint to verigy login credentials

--- site/citibike_stats.py
+++ site/citibike_stats.py
@@ -39,6 +39,29 @@ def get_trips():
     abort(500, "An unknown error occurred")
 
 
+@app.route('/verify-login', methods=['POST'])
+@ssl_required
+def get_trips():
+
+    # 'username' and 'password' are required
+    if 'username' not in request.form or 'password' not in request.form:
+        abort(400, "Must supply 'username' and 'password'")
+
+    username = request.form['username']
+    password = request.form['password']
+
+    try:
+
+        # attempt to login
+        NYCCitiBikeParser(username, password)
+
+        return "Success"
+
+    except NYCCitiBikeLoginError:
+        abort(403, "Could not log in with those credentials")
+
+    abort(500, "An unknown error occurred")
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
